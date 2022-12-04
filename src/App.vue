1adae77044b7b064f40da6be774d263a0355928d
jie.tong <jie.tong@huifu.com>
2022-12-04 23:16:16 +0800

    vue demo init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>31231232131231</div>
+</template>
+<script setup>
+import { Yami } from '../lib/main'
+console.log(Yami)
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => {
-  const config = {
-    build: {
-      lib: {
-        entry: './lib/main.ts',
-        name: 'Yami',
-        fileName: 'Yami',
-      },
-    },
-  }
-  if (mode === 'development') {
-    return config
-  } else {
-    return {
-      publicDir: false,
-      ...config,
-    }
-  }
+	const config = {
+		build: {
+			lib: {
+				entry: './lib/main.ts',
+				name: 'Yami',
+				fileName: 'Yami'
+			}
+		}
+	}
+	if (mode === 'development') {
+		return { ...config, plugins: [vue()] }
+	} else {
+		return {
+			publicDir: false,
+			...config
+		}
+	}
 })
